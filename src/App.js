735542e7b5a454b628a74159f55aed40c8a7afd9
omniboyok <barrea.pablo@gmail.com>
2021-03-16 01:30:15 -0300

    app endpoint data and structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import NavBar from "./main/Layout/NavBar";
+import InnerPage from "./main/Layout/InnerPage";
 
 function App() {
+  const [location, setLocation] = useState("");
+  useEffect(function () {
+    const getData = () => {
+      axios
+        .get("/v1/location")
+        .then((response) => {
+          setLocation(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    getData();
+
+    return null;
+  }, []);
   return (
     <div className="App">
-      <NavBar />
+      <NavBar location={location} />
+      <InnerPage />
     </div>
   );
 }
